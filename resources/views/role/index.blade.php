75729fd6610f9bc2e5025461b371ee83ca2b5f04
udinuyey <miftahfathurozy648@gmail.com>
2025-06-20 22:47:18 +0700

    views nontifikasi, role, user

---
+++ resources/views/role/index.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Presensi</title>
+</head>
+<body>
+    <h2>Ini halaman role</h2>
+</body>
+</html>
---
+++ resources/views/user/index.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Presensi</title>
+</head>
+<body>
+    <h2>Ini halaman user</h2>
+</body>
+</html>
